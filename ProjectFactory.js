34e42a45efd06ebda35e2b33ca682e69aeb543f1
PasinduLak <hgdpasindu@gmail.com>
2022-08-05 16:56:47 +0530

    refactor the code

--- ProjectFactory.js
+++ ProjectFactory.js
@@ -3,36 +3,36 @@ import { Project } from "./Project.js";
 
 export class ProjectFactory {
 
-
-
     constructor() {
         this.project = [];
     }
 
     async getAllProjects() {
-       
+
         let jsonRes;
-
-        let response = await fetch("./projects.json"); 
+        let response = await fetch("./projects.json");
         jsonRes = await response.json();
-
-        for(let i =0; i < jsonRes.projects.length; i++){
+        //await new Promise(r => setTimeout(r, 5000));
+        for (let i = 0; i < jsonRes.projects.length; i++) {
             this.project.push(this.createProject(jsonRes.projects[i].id, jsonRes.projects[i].name));
-        }        
+        }
 
         return this.project;
+
     }
 
     searchProjects(searchTerm) {
+
         let filtered = [];
         this.project.forEach(element => {
             let pName = element.name;
-
             if (pName.toLowerCase().search(searchTerm.toLowerCase()) >= 0) {
                 filtered.push(element);
             }
         });
+        
         return filtered;
+
     }
 
     createProject(id, name) {
